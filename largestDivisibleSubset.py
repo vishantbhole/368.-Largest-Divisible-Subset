e66cd26bd1a06acbbf6bc2622111a869493a10b5
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-04-09 06:14:01 -0500

    Add Solution class for largest divisible subset problem

--- largestDivisibleSubset.py
+++ largestDivisibleSubset.py
@@ -1,2 +1,7 @@
 #368. Largest Divisible Subset
 from typing import List
+
+class Solution:
+    def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
+        if not nums:
+            return []
