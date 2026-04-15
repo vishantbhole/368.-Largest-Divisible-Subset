fd5d95d3a341616ac9666b195affa870d39cea5a
Vishant <44126013+vishantbhole@users.noreply.github.com>
2026-04-15 06:34:42 -0500

    Correct logic for calculating max length of subset
    Fix indentation and return statement in the function.

--- largestDivisibleSubsetLength.py
+++ largestDivisibleSubsetLength.py
@@ -12,3 +12,6 @@
             for j in range(i):
                 if nums[i] % nums[j] == 0:
                     dp[i] = max(dp[i], dp[j] + 1)
+         max_len = max(max_len, dp[i])
+
+         return max_len
